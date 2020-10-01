68d7be9e8a28be245df9ff4488c8c831f80f2c20
Alonso Gutierrez <vn0jbpg@M-C02Z901ALVCG.local>
2020-09-30 22:50:48 -0300

    refactor(polyfill): add babel, registerserviceworker

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
+import 'react-app-polyfill/ie11'
+import 'react-app-polyfill/stable'
+import 'babel-polyfill'
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 //Component
 import './index.css'
 import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
 const Root = () => <App />
 
 ReactDOM.render(<Root />, document.getElementById('root'))
+
+registerServiceWorker()
