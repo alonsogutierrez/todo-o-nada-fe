514f11319521df91ce1048c8903cf03a0de10ad1
vn0jbpg <alonso.gutierrez@walmart.com>
2021-03-07 19:05:05 -0300

    feat(searchOrder): create new call to bff to get order by ordernumber, refactor in SearchOrder component to get order and filter, erase unnecesary invoice mockup

--- src/component/admin/SearchOrders/index.js
+++ src/component/admin/SearchOrders/index.js
@@ -2,124 +2,19 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import ReactTable from 'react-table'
 import { Col, Container, Row } from 'reactstrap'
-import invoice from '../../../api/invoice'
+import { format } from 'date-fns'
 
 import OrderDetailModal from './OrderDetailModal'
+import ClientAPI from '../../../common/ClientAPI'
 
 class SearchOrders extends Component {
   constructor(props) {
     super(props)
     this.state = {
       modal: false,
-      dropdownOpen: false,
-      isOpen: false,
-      invoices: invoice,
-      searchProduct: '',
-      invoiceview: '',
-      orderData: {
-        __v: 0,
-        _id: {
-          $oid: '6044eb64a53ff53f436159b6',
-        },
-        createdAt: {
-          $date: '2021-03-07T15:04:04.830Z',
-        },
-        inventoryState: {
-          confirmed: {
-            products: [],
-          },
-          reserved: {
-            products: [],
-          },
-        },
-        orderNumber: 78,
-        paymentData: {
-          state: 'created',
-          transaction: {
-            date: {
-              $date: '2021-03-07T15:04:04.441Z',
-            },
-            shipping: 1000,
-            subTotal: 1030,
-          },
-          user: {
-            address: {
-              address: 'Exequiel Fernandez 1575',
-              city: 'Región Metropolitana de Santiago',
-              commune: 'Cerrillos',
-              country: 'Chile',
-              num_address: 'depto 906',
-              zip_code: '',
-            },
-            email: 'alonso.gutierrez@mail.udp.cl',
-            firstName: 'Alonso',
-            lastName: 'Gutierrez',
-            phone: '989370610',
-          },
-        },
-        products: [
-          {
-            _id: {
-              $oid: '6044eb64a53ff53f436159b7',
-            },
-            itemNumber: 0,
-            name: "Women's Cozy Leisure Top",
-            prices: [
-              {
-                _id: {
-                  $oid: '6044eb64a53ff53f436159b8',
-                },
-                basePriceReference: 500,
-                basePriceSales: 500,
-                discount: 0,
-              },
-            ],
-            quantity: 1,
-            sku: 3,
-          },
-          {
-            _id: {
-              $oid: '6044eb64a53ff53f436159b9',
-            },
-            itemNumber: 0,
-            name: 'La-ta-da Boho Chic Head Wrap',
-            prices: [
-              {
-                _id: {
-                  $oid: '6044eb64a53ff53f436159ba',
-                },
-                basePriceReference: 330,
-                basePriceSales: 165,
-                discount: 50,
-              },
-            ],
-            quantity: 2,
-            sku: 2,
-          },
-          {
-            _id: {
-              $oid: '6044eb64a53ff53f436159bb',
-            },
-            itemNumber: 0,
-            name: "Women's Crochet Shoulder Chevron Sweatshirt",
-            prices: [
-              {
-                _id: {
-                  $oid: '6044eb64a53ff53f436159bc',
-                },
-                basePriceReference: 580,
-                basePriceSales: 200,
-                discount: 380,
-              },
-            ],
-            quantity: 1,
-            sku: 4,
-          },
-        ],
-        updatedAt: {
-          $date: '2021-03-07T15:04:04.830Z',
-        },
-      },
+      invoices: [],
+      searchText: '',
+      orderData: {},
       userData: {},
     }
 
@@ -132,42 +27,57 @@ class SearchOrders extends Component {
     })
   }
 
-  //TODO: Call to order API, and set userData and orderData from order
-  onSearchProduct(searchText) {
-    if (searchText === '') {
-      this.setState({
-        invoices: invoice,
-        searchProduct: searchText,
-      })
-    } else {
-      let SearchBuyer = invoice.filter((invo) => {
-        if (searchText === searchText.toLowerCase()) {
-          let buyer = invo.buyer.toLowerCase().indexOf(searchText.toLowerCase()) > -1
-          return buyer
-        } else {
-          let buyer = invo.buyer.toUpperCase().indexOf(searchText.toUpperCase()) > -1
-          return buyer
+  onSearchOrder(orderNumber) {
+    this.setState(
+      {
+        searchText: orderNumber,
+      },
+      async () => {
+        if (orderNumber != '') {
+          const clientAPI = new ClientAPI()
+          try {
+            const orderDataAPIResponse = await clientAPI.getOrderByOrderNumber(orderNumber)
+            if (orderDataAPIResponse.length > 0) {
+              const { orderNumber, paymentData, createdAt } = orderDataAPIResponse[0]
+              const userData = paymentData.user
+              const clientNames = userData.firstName + ' ' + userData.lastName
+              let orderDataTableResponse = {
+                orderNumber,
+                clientNames,
+                createdAt: format(new Date(createdAt), 'yyyy-MM-dd'),
+                orderStatus: paymentData.state,
+                paymentType: 'Webpay', //TODO: Change by real payment channel
+                totalPrice: paymentData.transaction.subTotal + paymentData.transaction.shipping,
+              }
+              this.setState({
+                userData,
+                orderData: orderDataAPIResponse[0],
+                invoices: [orderDataTableResponse],
+              })
+            } else {
+              this.setState({
+                userData: {},
+                orderData: {},
+                invoices: [],
+              })
+            }
+          } catch (err) {
+            console.error('Error trying to get order: ', err.message)
+          }
         }
-      })
-      this.setState({
-        searchProduct: searchText,
-        invoices: SearchBuyer,
-      })
-    }
+      }
+    )
   }
 
-  onViewInvoicePopup(data) {
-    this.setState({
-      invoiceview: data,
-    })
+  onViewInvoicePopup() {
     this.toggle()
   }
 
+  isEmptyUserData(userData) {
+    return Object.entries(userData).length > 0
+  }
+
   componentDidMount() {
-    //TODO: Call to BFF to get orders paid
-    this.setState({
-      userData: this.state.orderData.paymentData.user,
-    })
     window.scrollTo(0, 0)
   }
 
@@ -175,54 +85,46 @@ class SearchOrders extends Component {
     const columns = [
       {
         maxWidth: 75,
-        Header: 'No.',
-        accessor: 'id',
-      },
-      {
-        sortable: false,
-        Header: 'Transaction Id',
-        accessor: 'invoiceid',
+        Header: 'Nº orden',
+        accessor: 'orderNumber',
       },
       {
         minWidth: 160,
-        Header: 'Buyer',
-        accessor: 'buyer',
+        Header: 'Cliente',
+        accessor: 'clientNames',
       },
       {
-        Header: 'Date',
-        accessor: 'date',
+        Header: 'Fecha compra',
+        accessor: 'createdAt',
       },
       {
-        Header: 'Status',
-        accessor: 'status',
+        Header: 'Estado',
+        accessor: 'orderStatus',
       },
       {
-        Header: 'Payment',
-        accessor: 'payment',
+        Header: 'Tipo de pago',
+        accessor: 'paymentType',
       },
       {
-        Header: 'Price',
-        accessor: 'price',
+        Header: 'Total',
+        accessor: 'totalPrice',
       },
       {
-        Header: 'Debit',
-        accessor: 'debit',
-      },
-      {
-        Header: 'Action',
+        Header: 'Acciones',
         accessor: 'action',
         Cell: (props) => {
           return (
             <div>
               <Link className="view-button" onClick={() => this.onViewInvoicePopup(props.original)}>
                 {' '}
-                View <i className="fa fa-eye pl-2"></i>
+                Ver <i className="fa fa-eye pl-2"></i>
               </Link>
             </div>
           )
         },
       },
     ]
+    const { searchText, userData, orderData, invoices, modal } = this.state
 
     return (
       <div>
@@ -240,26 +142,30 @@ class SearchOrders extends Component {
                         type="text"
                         className="form-control"
                         placeholder="Buscador de ordenes"
-                        value={this.state.searchProduct}
-                        onChange={(e) => this.onSearchProduct(e.target.value)}
+                        value={searchText}
+                        onChange={(e) => this.onSearchOrder(e.target.value)}
                       ></input>
                     </div>
                   </form>
                 </div>
                 <ReactTable
                   className="invoices-table"
-                  data={this.state.invoices}
+                  data={invoices}
                   columns={columns}
                   minRows={1}
                   defaultPageSize={5}
                 />
                 {/* modal-view */}
-                <OrderDetailModal
-                  userData={this.state.userData}
-                  orderData={this.state.orderData}
-                  openModal={this.state.modal}
-                  setToggle={this.toggle}
-                />
+                {this.isEmptyUserData(userData) ? (
+                  <OrderDetailModal
+                    userData={userData}
+                    orderData={orderData}
+                    openModal={modal}
+                    setToggle={this.toggle}
+                  />
+                ) : (
+                  <></>
+                )}
               </Col>
             </Row>
           </Container>
