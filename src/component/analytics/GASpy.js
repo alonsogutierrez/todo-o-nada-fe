210e506a7ee612d9be7ceb66592c86c56a9f1b93
alonsogutierrez <alonso.gutierrez@mail.udp.cl>
2021-11-17 23:53:04 -0300

    feat(ga): add google analytics id with env variable

--- src/component/analytics/GASpy.js
+++ src/component/analytics/GASpy.js
@@ -3,7 +3,7 @@ import { withRouter } from 'react-router-dom'
 import ReactGA from 'react-ga'
 import PropTypes from 'prop-types'
 
-const GOOGLE_ANALYTICS_ID = 'G-ZF4D2DYLKK' //set this var in env vars
+const GOOGLE_ANALYTICS_ID = process.env.GOOGLE_ANALYTICS_ID //set this var in env vars
 
 class Spy extends React.Component {
   constructor(props) {
