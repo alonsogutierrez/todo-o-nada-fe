ff5ad8b381a0539bda7d2e740b39e4945a1d184e
alonsogutierrez <alonso.gutierrez@mail.udp.cl>
2022-04-28 19:14:43 -0400

    feat(slider): add config to slider pass from banners autmatically

--- src/component/home/HomSlider.js
+++ src/component/home/HomSlider.js
@@ -4,10 +4,11 @@ import Slider from 'react-slick'
 const settings = {
   dots: false,
   infinite: true,
-  speed: 500,
-  arrows: true,
   slidesToShow: 1,
   slidesToScroll: 1,
+  autoplay: true,
+  speed: 2000,
+  autoplaySpeed: 5000,
 }
 
 const HomSlider = () => {
@@ -18,17 +19,17 @@ const HomSlider = () => {
         <div className="slide-inner">
           <div className="slide-image">
             <img
-              src={require(`../../assets/images/home-slider/banner_shuter_doji-05.jpg`).default}
+              src={require(`../../assets/images/home-slider/banner_tattoo_catalina-01.jpg`).default}
               alt="slide-1"
             />
           </div>
         </div>
       </div>
-      <div key={2} className="slide-04-item">
+      <div key={2} className="slide-04-item" onClick={() => handleOnClickBanner()}>
         <div className="slide-inner">
           <div className="slide-image">
             <img
-              src={require(`../../assets/images/home-slider/irezumi_banner-01.jpg`).default}
+              src={require(`../../assets/images/home-slider/banner_shuter_doji-05.jpg`).default}
               alt="slide-2"
             />
           </div>
@@ -38,8 +39,18 @@ const HomSlider = () => {
         <div className="slide-inner">
           <div className="slide-image">
             <img
-              src={require(`../../assets/images/home-slider/soul_of_dragon_banner-01.jpg`).default}
+              src={require(`../../assets/images/home-slider/irezumi_banner-01.jpg`).default}
               alt="slide-3"
+            />
+          </div>
+        </div>
+      </div>
+      <div key={4} className="slide-04-item">
+        <div className="slide-inner">
+          <div className="slide-image">
+            <img
+              src={require(`../../assets/images/home-slider/soul_of_dragon_banner-01.jpg`).default}
+              alt="slide-4"
             />
           </div>
         </div>
