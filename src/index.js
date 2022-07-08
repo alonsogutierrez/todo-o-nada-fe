660b8745dc3d59632dcf965014708127a9897414
alonsogutierrez <alonso.gutierrez@mail.udp.cl>
2022-07-07 23:17:32 -0400

    fix(service-workers): turn off service workers

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ import ReactDOM from 'react-dom'
 
 import './index.css'
 import App from './App'
-import registerServiceWorker from './registerServiceWorker'
+// import registerServiceWorker from './registerServiceWorker'
 
 const Root = () => <App />
 
 ReactDOM.render(<Root />, document.getElementById('root'))
 
-registerServiceWorker()
+// registerServiceWorker()
